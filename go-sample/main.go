147a4fbca333b7af6e25f1c60f45b5ac527260ec
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2019-01-10 00:19:13 +0000

    Add go-sample

---
+++ go-sample/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Print("Web server is available at port :8080")
+}
